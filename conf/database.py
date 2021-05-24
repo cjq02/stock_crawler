57006ff7a7b465decd52f60fad5a8468faf51a15
cjq <cjq02@qq.com>
2021-05-24 10:46:09 +0800

    Database Config

--- conf/database.py
+++ conf/database.py
@@ -9,34 +9,41 @@ Created on 2021/04/15
 
 import datetime
 import uuid
-
+import json
 import psycopg2
 
 
 def get_conn():
-    return psycopg2.connect(database="", user="", password="", host="", port="5432")
+    json_file = open('./conf/conn.json')
+    conn_info = json.load(json_file)
+    print(conn_info['database'])
+    return psycopg2.connect(database=conn_info['database'], user=conn_info['user'], password=conn_info['password'], host=conn_info['host'], port=conn_info['port'])
 
 
 def execute_sql(sql):
     conn = get_conn()
     cursor = conn.cursor()
-    
+
     cursor.execute(sql)
 
     conn.commit()
     cursor.close()
     conn.close()
 
+
 def batch_execute(sql_list):
     conn = get_conn()
     cursor = conn.cursor()
-    
-    print('共{}条sql，开始执行...'.format(len(sql_list)))
+
+    sql_length = len(sql_list)
+    print('共{}条sql，开始执行...'.format(sql_length))
 
     index = 0
     for sql in sql_list:
         index += 1
-        print('{} 执行第{}条sql：{}'.format(datetime.datetime.now(), index, sql))
+        remain_length = sql_length-index
+        print('{} 执行第{}条sql（剩余{}条）：{}'.format(
+            datetime.datetime.now(), index, remain_length, sql))
         cursor.execute(sql)
 
     conn.commit()
